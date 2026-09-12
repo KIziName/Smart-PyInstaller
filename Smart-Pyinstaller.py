b62d10a5b91970a6734a48cffad89cf9a18f754d
KiziName <yyddtf576@gmail.com>
2026-09-12 13:49:37 +0300

    Update Smart-Pyinstaller.py

--- Smart-Pyinstaller.py
+++ Smart-Pyinstaller.py
@@ -7,6 +7,7 @@ import time
 
 from dataclasses import dataclass
 from pathlib import Path
+from __future__ import annotations
 
 ICON_SIZES = [(256, 256), (128, 128), (64, 64), (48, 48), (32, 32), (16, 16)]
 IMAGE_EXTS = ["*.png", "*.jpg", "*.jpeg", "*.bmp", "*.webp"]
